fb3b23832c4db2c1fddb773902f0378e1eda6470
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-02-11 15:29:36 +0100

    Add display all quizzes

--- src/pages/api/quiz.js
+++ src/pages/api/quiz.js
@@ -14,7 +14,8 @@ export default async function handler(req, res) {
 					const quizzes = await Quiz.find({ _id: id });
 					res.status(200).json({ data: quizzes });
 				} else {
-					res.status(500).json({ error: "Missing ID" });
+					const quizzes = await Quiz.find();
+					res.status(200).json({ data: quizzes });
 				}
 			} catch (error) {
 				res.status(500).json({ error });
--- src/pages/questioner/index.js
+++ src/pages/questioner/index.js
@@ -1,14 +1,40 @@
 import Link from "next/link";
 
-export default function Questioner() {
+export default function Questioner(props) {
 	return (
 		<>
 			<div>
 				<Link href="/questioner/quiz/create">Create new Quiz</Link>
 			</div>
 			<div>
-				<Link href="/questioner/quiz/edit">Edit Quiz</Link>
+				{props.quizzes.length > 0 &&
+					props.quizzes.map(quiz => (
+						<div key={quiz._id}>
+							<p>{quiz.name}</p>
+							<p>{quiz.description}</p>
+							<p>{quiz.duration}</p>
+							<Link href={`/questioner/quiz/edit?id=${quiz._id}`}>Edit Quiz</Link>
+						</div>
+					))}
 			</div>
 		</>
 	);
+}
+
+export async function getServerSideProps() {
+	const quizzes = await fetch(process.env.SITE_URI + "/api/quiz", {
+		method: "GET",
+		redirect: "follow",
+	})
+		.then(response => response.json())
+		.then(result => {
+			return result.data;
+		})
+		.catch(error => console.log("error", error));
+
+	return {
+		props: {
+			quizzes,
+		},
+	};
 }
