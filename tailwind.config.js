cbf3ed4db29a4963ab3c2c2d752a928e0383d5b4
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-02-23 19:44:20 +0100

    Add Tailwind and Preline

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"node_modules/preline/dist/*.js",
+
+		// Or if using `src` directory:
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("preline/plugin")],
+};
