abdc660644e4ad127670c8fb2731ae39221ac897
Marija Ćehić <cehicmarija@gmail.com>
2023-02-25 23:02:35 +0100

    Update Navigation responsive styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,10 +7,15 @@ import logo from "../../public/images/recruitem-logo.png";
 export default function Home() {
 	return (
 		<div className="bg-recruit-blue text-white">
-			<div className="px-6 flex items-center justify-between ">
+			<div className="px-2 md:px-6 py-2 md:py-0 flex items-center justify-between">
 				<Link href="/">
 					{" "}
-					<Image src={logo} alt="Recruitem logo" height={55} />{" "}
+					<Image
+						className="w-3/4 md:w-full"
+						src={logo}
+						alt="Recruitem logo"
+						height={55}
+					/>{" "}
 				</Link>
 
 				<nav>
