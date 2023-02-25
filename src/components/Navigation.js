1e0a2cbb5c28a452cd8336d9bbd73c56ef1b13c1
Marija Ćehić <cehicmarija@gmail.com>
2023-02-25 22:57:08 +0100

    Update navigation links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { VscAccount, VscActivateBreakpoints } from "react-icons/vsc";
 
 import logo from "../../public/images/recruitem-logo.png";
@@ -7,18 +8,21 @@ export default function Home() {
 	return (
 		<div className="bg-recruit-blue text-white">
 			<div className="px-6 flex items-center justify-between ">
-				<Image className="w-" src={logo} alt="Recruitem logo" height={55} />
+				<Link href="/">
+					{" "}
+					<Image src={logo} alt="Recruitem logo" height={55} />{" "}
+				</Link>
 
 				<nav>
 					<ul className="flex gap-8">
-						<li className="flex items-center gap-2">
+						<Link href="/recruiter" className="flex items-center gap-2">
 							<VscActivateBreakpoints />
 							Recruiter
-						</li>
-						<li className="flex items-center gap-2">
+						</Link>
+						<Link href="/candidate" className="flex items-center gap-2">
 							<VscAccount />
 							Candidate
-						</li>
+						</Link>
 					</ul>
 				</nav>
 			</div>
