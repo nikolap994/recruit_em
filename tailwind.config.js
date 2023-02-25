9b5002bf9efc362a5d2f392450d27efab57ea5e7
Marija Ćehić <cehicmarija@gmail.com>
2023-02-25 22:50:57 +0100

    Add recruit-blue color class

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,15 @@ module.exports = {
 		"./src/pages/**/*.{js,ts,jsx,tsx}",
 		"./src/components/**/*.{js,ts,jsx,tsx}",
 		"node_modules/preline/dist/*.js",
-
 		// Or if using `src` directory:
 		"./src/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"recruit-blue": "#1c1a4d",
+			},
+		},
 	},
 	plugins: [require("preline/plugin")],
 };
