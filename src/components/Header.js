fe33a79b42ac7d9dc8cf6f28c293cb1dccfb8ac7
Marija Ćehić <cehicmarija@gmail.com>
2023-02-25 18:21:25 +0100

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import Navigation from "./Navigation";
+function Header() {
+	return (
+		<header className="sticky z-50 w-full">
+			<Navigation />
+		</header>
+	);
+}
+
+export default Header;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,15 @@ import "@/styles/globals.css";
 import { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 
+import Header from "@/components/Header";
+
 export default function App({ Component, pageProps }) {
 	useEffect(() => {
 		import("preline");
 	}, []);
 	return (
 		<SessionProvider>
+			<Header />
 			<Component {...pageProps} />
 		</SessionProvider>
 	);
