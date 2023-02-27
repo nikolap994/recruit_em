36c2bf37668f7127f08a21e58ec4af838fc8fc55
Marija Ćehić <cehicmarija@gmail.com>
2023-02-27 22:50:20 +0100

    Update Hero styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,30 +3,38 @@ import RotateImages from "./RotateImages";
 
 function Hero() {
 	return (
-		<section className="flex">
-			<div className="w-1/2">Hero</div>
-			<div className="flex w-1/2 rotate-[32deg]">
-				<RotateImages
-					className="h-2"
-					imageOne={promoImg}
-					imageTwo={promoImg}
-					imageThree={promoImg}
-					imageFour={promoImg}
-				/>
-				<RotateImages
-					className="h-2"
-					imageOne={promoImg}
-					imageTwo={promoImg}
-					imageThree={promoImg}
-					imageFour={promoImg}
-				/>
-				<RotateImages
-					className="h-2"
-					imageOne={promoImg}
-					imageTwo={promoImg}
-					imageThree={promoImg}
-					imageFour={promoImg}
-				/>
+		<section className="flex relative h-[50vh] overflow-hidden bg-indigo-200 p-[100px]">
+			<div className="w-1/2 flex flex-col justify-center items-center z-[2] backdrop-opacity-20 bg-white/30 backdrop-invert">
+				<h1 className="text-[100px]">RecruitEm</h1>
+				<p className="text-4xl px-8">
+					The only recruiting tool you'll ever need
+				</p>
+			</div>
+			<div className="flex w-1/2 rotate-[32deg] absolute right-[10%] -top-[200px]">
+				<div className="animate-carousel">
+					<RotateImages
+						imageOne={promoImg}
+						imageTwo={promoImg}
+						imageThree={promoImg}
+						imageFour={promoImg}
+					/>
+				</div>
+				<div className="animate-carouselTwo">
+					<RotateImages
+						imageOne={promoImg}
+						imageTwo={promoImg}
+						imageThree={promoImg}
+						imageFour={promoImg}
+					/>
+				</div>
+				<div className="animate-carouselThree">
+					<RotateImages
+						imageOne={promoImg}
+						imageTwo={promoImg}
+						imageThree={promoImg}
+						imageFour={promoImg}
+					/>
+				</div>
 			</div>
 		</section>
 	);
