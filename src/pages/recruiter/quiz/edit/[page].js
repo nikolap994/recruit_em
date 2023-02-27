d38a5a0e48eaef14d581ed93c2bff5ce2626a4a5
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-02-27 20:22:55 +0100

    Update quiz edit form

--- src/pages/recruiter/quiz/edit/[page].js
+++ src/pages/recruiter/quiz/edit/[page].js
@@ -1,10 +1,51 @@
 import { getSession } from "next-auth/react";
 
+const submitForm = async e => {};
+const getQuestion = async id => {
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+
+	var requestOptions = {
+		method: "GET",
+		headers: myHeaders,
+		redirect: "follow",
+	};
+
+	return await fetch(
+		process.env.SITE_URI + "/api/question?id=" + id,
+		requestOptions
+	)
+		.then(response => response.json())
+		.then(result => result)
+		.catch(error => console.log("error", error));
+};
+
 export default function EditQuiz(props) {
 	return (
 		<>
-			Edit Quiz
-			{console.log(props)}
+			<form method="POST" onSubmit={submitForm}>
+				<input type="text" name="name" defaultValue={props.name}></input>
+				<input
+					type="number"
+					name="duration"
+					defaultValue={props.duration}
+				></input>
+				<textarea
+					type="text"
+					name="description"
+					defaultValue={props.description}
+				></textarea>
+
+				<div>
+					{props.questions.length > 0 &&
+						props.questions.map(question => (
+							<div key={question[0]._id}>
+								<div>{question[0]._id}</div>
+								<div>{question[0].question}</div>
+							</div>
+						))}
+				</div>
+			</form>
 		</>
 	);
 }
@@ -30,28 +71,35 @@ export async function getServerSideProps(context) {
 			redirect: "follow",
 		};
 
+		let questionList = [];
+
 		const response = await fetch(
 			process.env.SITE_URI + "/api/quiz?id=" + quizId,
 			requestOptions
 		)
 			.then(response => response.json())
 			.then(result => {
-				return {
-					props: {
-						quizId: quizId,
-						name: result.data[0].name,
-						duration: result.data[0].duration,
-						questions: result.data[0].questions,
-						description: result.data[0].description,
-					},
-				};
+				return result.data;
 			})
 			.catch(error => console.log("error", error));
 
-		return response;
-	}
+		let questionListFinal = await Promise.all(
+			response[0].questions.map(async question => {
+				return getQuestion(question).then(result => {
+					questionList.push(result.data[0]);
+					return questionList;
+				});
+			})
+		);
 
-	return {
-		props: {},
-	};
+		return {
+			props: {
+				quizId: quizId,
+				name: response[0].name,
+				duration: response[0].duration,
+				questions: questionListFinal,
+				description: response[0].description,
+			},
+		};
+	}
 }
