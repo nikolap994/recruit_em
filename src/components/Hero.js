23ee1658b88b93d6684d922ccc6a5d2f7dad6fa7
Marija Ćehić <cehicmarija@gmail.com>
2023-03-13 21:10:27 +0100

    Update Hero mobile styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,10 @@ import RotateImages from "./RotateImages";
 
 function Hero() {
 	return (
-		<section className="flex relative h-[50vh] overflow-hidden bg-indigo-200 p-[100px]">
-			<div className="w-1/2 flex flex-col justify-center items-center z-[2] px-8 text-left">
-				<h1 className="text-[100px]">RecruitEm</h1>
-				<p className="text-4xl pl-5">
+		<section className="flex relative h-[50vh] overflow-hidden bg-indigo-200 lg:p-[100px]">
+			<div className="lg:w-1/2 flex flex-col justify-center items-center z-[2] px-8 text-center lg:text-left">
+				<h1 className="text-6xl lg:text-[100px]">RecruitEm</h1>
+				<p className="text-4xl lg:pl-5">
 					The only recruiting tool you'll ever need
 				</p>
 			</div>
