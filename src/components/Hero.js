90c6b814293746b54092f6f541977bd67b414aad
Marija Ćehić <cehicmarija@gmail.com>
2023-03-13 20:47:28 +0100

    Update Hero padding, animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,21 +4,13 @@ import RotateImages from "./RotateImages";
 function Hero() {
 	return (
 		<section className="flex relative h-[50vh] overflow-hidden bg-indigo-200 p-[100px]">
-			<div className="w-1/2 flex flex-col justify-center items-center z-[2] backdrop-opacity-20 bg-white/30 backdrop-invert">
+			<div className="w-1/2 flex flex-col justify-center items-center z-[2] px-8 text-left">
 				<h1 className="text-[100px]">RecruitEm</h1>
-				<p className="text-4xl px-8">
+				<p className="text-4xl pl-5">
 					The only recruiting tool you'll ever need
 				</p>
 			</div>
 			<div className="flex w-1/2 rotate-[32deg] absolute right-[10%] -top-[200px]">
-				<div className="animate-carousel">
-					<RotateImages
-						imageOne={promoImg}
-						imageTwo={promoImg}
-						imageThree={promoImg}
-						imageFour={promoImg}
-					/>
-				</div>
 				<div className="animate-carouselTwo">
 					<RotateImages
 						imageOne={promoImg}
