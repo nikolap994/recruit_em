8fa0eb78bbbef8dd15bee85df9a4f6d1854eb46d
Marija Ćehić <cehicmarija@gmail.com>
2023-03-22 23:49:09 +0100

    Add Open Position button

--- src/components/OpenPosition.js
+++ src/components/OpenPosition.js
@@ -1,8 +1,19 @@
-function OpenPosition({ positionTitle, positionShortDescription }) {
+import Link from "next/link";
+function OpenPosition({
+	positionTitle,
+	positionShortDescription,
+	positionLink,
+}) {
 	return (
-		<div className="bg-indigo-400 text-white py-16 px-6 rounded-3xl">
-			<h3 className="text-2xl mb-4">{positionTitle}</h3>
+		<div className="bg-indigo-400 text-white py-16 px-6 rounded-3xl flex flex-col gap-4">
+			<h3 className="text-2xl">{positionTitle}</h3>
 			<p>{positionShortDescription}</p>
+			<Link
+				className="bg-white text-black p-4 self-center mt-7 rounded-lg"
+				href={positionLink}
+			>
+				Apply now{" "}
+			</Link>
 		</div>
 	);
 }
