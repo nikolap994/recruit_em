531f5681a551bd70ab9ece459688a0d6092df075
Marija Ćehić <cehicmarija@gmail.com>
2023-03-22 22:52:46 +0100

    Update Resume responsive styles

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,18 +8,25 @@ function Resume() {
 
 	return (
 		<div className="relative bg-indigo-900 py-12">
-			<h1 className="text-center text-white text-6xl pt-10 pb-8">
-				Transparent hiring. Always.
-			</h1>
+			<div className="text-center text-white pt-10 pb-16 lg:pb-8">
+				<h1 className="text-5xl lg:text-6xl pb-6">
+					Transparent hiring. Always.
+				</h1>
+				<p className="lg:w-1/2 mx-auto px-6 lg:px-0">
+					RecruitEm allows you to be in control of all parts of the hiring
+					process - and bring the candidates skills and traits to the light.
+				</p>
+			</div>
 			<Image
-				className="lg:w-1/2 lg:mx-auto lg:my-12"
+				className="lg:w-1/2 lg:mx-auto lg:my-12 transition-opacity absolute"
 				src={cvImage}
 				alt="John Doe resume"
 			/>
 			<Image
 				onMouseEnter={() => setHover(true)}
 				onMouseLeave={() => setHover(false)}
-				className={`absolute top-0 lg:w-1/2 top-[10rem] lg:left-[25%] ${
+				onTouchMove={() => setHover(true)}
+				className={`absolute top-0 lg:w-1/2 top-[21rem] lg:top-[15rem] xl:top-[18.8rem] lg:left-[25%] transition-opacity ${
 					isHovered ? "opacity-0" : "opacity-1"
 				}`}
 				src={cvImage2}
