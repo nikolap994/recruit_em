4ac7c18dab3ac2cd6803c07e0621a5547dc910b9
Marija Ćehić <cehicmarija@gmail.com>
2023-04-18 20:33:44 +0200

    Update Recruiter navigation hover styles

--- src/components/RecruiterNavigation.js
+++ src/components/RecruiterNavigation.js
@@ -1,38 +1,51 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+	const router = useRouter();
+
 	return (
 		<div className="bg-recruit-blue text-white">
-			<div className="px-2 md:px-6 py-2 md:py-0 flex items-center justify-between">
+			<div className="px-2 md:px-6 py-6 md:py-0 flex items-center justify-between">
 				<nav>
 					<ul className="flex gap-8">
 						<Link
 							href="/recruiter/"
-							className="flex items-center gap-2 hover:border-b-[1px]"
+							className={`${
+								router.pathname == "/recruiter" ? "font-semibold" : ""
+							} hover:font-semibold`}
 						>
 							Dashboard
 						</Link>
 						<Link
 							href="/recruiter/quiz"
-							className="flex items-center gap-2 hover:border-b-[1px]"
+							className={`${
+								router.pathname == "/recruiter/quiz" ? "font-semibold" : ""
+							} hover:font-semibold`}
 						>
 							Quizzes
 						</Link>
 						<Link
 							href="/recruiter/positions"
-							className="flex items-center gap-2 hover:border-b-[1px]"
+							className={`${
+								router.pathname == "/recruiter/positions" ? "font-semibold" : ""
+							} hover:font-semibold`}
 						>
 							Positions
 						</Link>
 						<Link
 							href="/recruiter/reviews"
-							className="flex items-center gap-2 hover:border-b-[1px]"
+							className={`${
+								router.pathname == "/recruiter/reviews" ? "font-semibold" : ""
+							} hover:font-semibold`}
 						>
 							Reviews
 						</Link>
 						<Link
 							href="/recruiter/users"
-							className="flex items-center gap-2 hover:border-b-[1px]"
+							className={`${
+								router.pathname == "/recruiter/users" ? "font-semibold" : ""
+							} hover:font-semibold`}
 						>
 							Users
 						</Link>
