de2b942e42f929cd83a63d092e2c8d5aaf531306
Marija Ćehić <cehicmarija@gmail.com>
2023-04-18 21:09:32 +0200

    Update Quiz Dashboard styles

--- src/pages/recruiter/quiz/index.js
+++ src/pages/recruiter/quiz/index.js
@@ -13,26 +13,30 @@ export default function Quiz(props) {
 
 			<RecruiterNavigation />
 
-			<div className="max-w-7xl mx-auto px-4 md:px-6">
-				<div className="pt-10 flex flex-col md:flex-row max-w-lg justify-between md:items-center mb-8 lg:mb-12">
-					<h1 className="text-5xl my-8 md:my-16">Quiz Dashboard</h1>
-
+			<div className="max-w-7xl mx-auto px-4 md:px-6 border border-blue-900 rounded-3xl md:h-screen relative mt-10">
+				<div className="md:pt-10 mb-8 lg:mb-12 border border-blue-900 rounded-3xl bg-indigo-600 text-white text-center">
+					<h1 className="text-3xl text-5xl my-8 my-16">Quiz Dashboard</h1>
+				</div>
+				<div className="max-w-7xl w-full left-1/2 flex justify-center">
 					<Link
-						className="text-white bg-blue-700 rounded px-8 py-4 text-center mb-5 md:mb-0"
+						className="md:absolute top-[190px] bg-white rounded-md border border-indigo-900 text-indigo-900 font-semibold px-8 py-4 text-center mb-5 md:mb-0 "
 						href="/recruiter/quiz/create"
 					>
 						Create new Quiz
 					</Link>
 				</div>
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5 pb-16">
+				<div className="grid lg:grid-cols-2 items-center md:pb-16 md:mt-10">
 					{props.quizzes.length > 0 &&
-						props.quizzes.map(quiz => (
-							<div className="px-6 py-4" key={quiz._id}>
-								<p>{quiz.name}</p>
+						props.quizzes.map((quiz) => (
+							<div
+								className="px-6 py-4 border border-white border-b-indigo-900 mt-7"
+								key={quiz._id}
+							>
+								<h3 className="text-xl">{quiz.name}</h3>
 								<p>{quiz.description}</p>
-								<p>{quiz.duration}</p>
+								<p className="text-indigo-600">{quiz.duration} minutes</p>
 								<Link
-									className="text-white bg-blue-700 rounded text-center w-full inline-block pt-2 pb-2"
+									className="text-white bg-blue-700 rounded text-center w-full inline-block pt-2 pb-2 max-w-lg"
 									href={`/recruiter/quiz/edit/${quiz._id}`}
 								>
 									Edit Quiz
@@ -52,11 +56,11 @@ export async function getServerSideProps(context) {
 		method: "GET",
 		redirect: "follow",
 	})
-		.then(response => response.json())
-		.then(result => {
+		.then((response) => response.json())
+		.then((result) => {
 			return result.data;
 		})
-		.catch(error => console.log("error", error));
+		.catch((error) => console.log("error", error));
 
 	return {
 		props: {
