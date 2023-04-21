b7508e2e2045dcf9d8387b89d1b78657358531a7
Marija Ćehić <cehicmarija@gmail.com>
2023-04-21 21:08:06 +0200

    Update Edit Position styles

--- src/pages/recruiter/positions/edit/[page].js
+++ src/pages/recruiter/positions/edit/[page].js
@@ -1,7 +1,7 @@
 import { getSession } from "next-auth/react";
 import Head from "next/head";
 
-const updatePosition = async e => {
+const updatePosition = async (e) => {
 	e.preventDefault();
 
 	const id = e.target.id.value;
@@ -31,9 +31,9 @@ const updatePosition = async e => {
 	};
 
 	fetch(process.env.SITE_URI + "/api/position", requestOptions)
-		.then(response => response.text())
-		.then(result => console.log(result))
-		.catch(error => console.log("error", error));
+		.then((response) => response.text())
+		.then((result) => console.log(result))
+		.catch((error) => console.log("error", error));
 
 	console.log(id, name, status, description, quiz);
 };
@@ -45,21 +45,25 @@ export default function EditPosition(props) {
 				<title>Update Position</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
+			<h1 className="text-5xl py-8 md:py-16 bg-indigo-600 text-white text-center">
+				Edit Position
+			</h1>
 			<form
-				className="max-w-7xl mx-auto px-4 md:px-6"
+				className="max-w-7xl mx-auto px-4 md:px-6 flex flex-col"
 				method="POST"
 				onSubmit={updatePosition}
 			>
-
-				<h1 className="text-5xl my-8 md:my-16">Update Position</h1>
 				<input
 					type="hidden"
 					name="id"
 					defaultValue={props.position[0]._id}
 				></input>
-				<div>
-					<label htmlFor="name">Name</label>
+				<div className="h-12 flex gap-4 border border-black justify-around pl-2 mt-10">
+					<label className="text-xl w-1/2 flex items-center" htmlFor="name">
+						Name
+					</label>
 					<input
+						className="pl-2 border-black border-l text-gray-600 italic w-1/2"
 						required
 						type="text"
 						name="name"
@@ -67,9 +71,12 @@ export default function EditPosition(props) {
 						defaultValue={props.position[0].name}
 					></input>
 				</div>
-				<div>
-					<label htmlFor="description">Description</label>
+				<div className="flex items-center gap-4 border border-black border-t-0 justify-around pl-2">
+					<label className="w-1/2" htmlFor="description">
+						Description
+					</label>
 					<textarea
+						className="py-2 pl-2 border-black border-l text-gray-600 italic w-1/2"
 						required
 						type="text"
 						name="description"
@@ -77,19 +84,35 @@ export default function EditPosition(props) {
 						defaultValue={props.position[0].description}
 					></textarea>
 				</div>
-				<div>
+				<div className="flex items-center gap-12 pl-2 w-1/2 mt-6">
 					<label htmlFor="status">Status</label>
-					<select name="status" id="status" required defaultValue={props.position[0].status} multiple={false}>
-						<option value="enabled">Enabled</option>
-						<option value="disabled">Disabled</option>
+					<select
+						name="status"
+						id="status"
+						required
+						defaultValue={props.position[0].status}
+						multiple={false}
+					>
+						<option className="bg-green-500" value="enabled">
+							Enabled
+						</option>
+						<option value="disabled bg-red-500 hover:bg-red-700">
+							Disabled
+						</option>
 					</select>
 				</div>
 
-				<div>
+				<div className="flex items-center gap-16 pl-2 w-1/2 mt-6">
 					<label htmlFor="quiz">Quiz</label>
-					<select name="quiz" id="quiz" required defaultValue={props.position[0].quiz} multiple={false}>
+					<select
+						name="quiz"
+						id="quiz"
+						required
+						defaultValue={props.position[0].quiz}
+						multiple={false}
+					>
 						{props.quizzes.length > 0 &&
-							props.quizzes.map(quiz => (
+							props.quizzes.map((quiz) => (
 								<option key={quiz._id} value={quiz._id}>
 									{quiz.name}
 								</option>
@@ -97,7 +120,7 @@ export default function EditPosition(props) {
 					</select>
 				</div>
 				<button
-					className="text-white bg-blue-700 rounded text-center w-full inline-block pt-2 pb-2"
+					className="mt-6 w-1/2 text-white bg-blue-700 rounded text-center inline-block pt-2 pb-2"
 					type="submit"
 				>
 					Update Position
@@ -132,21 +155,21 @@ export async function getServerSideProps(context) {
 			process.env.SITE_URI + "/api/position?id=" + positionId,
 			requestOptions
 		)
-			.then(response => response.json())
-			.then(result => {
+			.then((response) => response.json())
+			.then((result) => {
 				return result.data;
 			})
-			.catch(error => console.log("error", error));
+			.catch((error) => console.log("error", error));
 
 		const quizzes = await fetch(process.env.SITE_URI + "/api/quiz", {
 			method: "GET",
 			redirect: "follow",
 		})
-			.then(response => response.json())
-			.then(result => {
+			.then((response) => response.json())
+			.then((result) => {
 				return result.data;
 			})
-			.catch(error => console.log("error", error));
+			.catch((error) => console.log("error", error));
 
 		return {
 			props: {
