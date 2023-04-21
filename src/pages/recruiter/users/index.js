683eab7ba23e25230bba78631a5263620572f04d
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-04-21 22:08:05 +0200

    Update layout for users page

--- src/pages/recruiter/users/index.js
+++ src/pages/recruiter/users/index.js
@@ -12,28 +12,43 @@ export default function Users(props) {
 			</Head>
 
 			<RecruiterNavigation />
-			<div className="md:pb-4 md:mt-20 lg:mt-12 border border-t-0 border-l-0 border-r-0 border-t-indigo-900 md:border-t-white border-b-indigo-900 md:w-[80vw] mx-auto">
-				{props.users.length > 0 &&
-					props.users.map(user => (
-						<div
-							className="px-6 py-6 md:pb-8 grid grid-flow-col gap-1 lg:w-3/4 mx-auto md:justify-between"
-							key={user._id}
-						>
-							<div>
-								<h3 className="text-xl">
-									{user.firstName} {user.lastName}
-								</h3>
-								<p>Role: {user.role}</p>
-							</div>
-
-							<Link
-								className="hover:animate-pulse self-start text-white bg-blue-700 rounded text-center md:w-56 inline-block py-2 px-10 lg:mt-6 md:mt-0 max-w-lg"
-								href={`/recruiter/users/edit/${user._id}`}
+			<div className="md:max-w-[89vw] lg:max-w-7xl mx-auto md:border border-blue-900 md:rounded-3xl md:h-screen relative md:mt-10">
+				<div className="md:pt-10 lg:mb-12 border border-blue-900 md:rounded-3xl bg-indigo-600 text-white text-center">
+					<h1 className="text-xl md:text-3xl text-5xl my-8 md:my-16">
+						User Dashboard
+					</h1>
+				</div>
+				<div className="max-w-7xl w-full left-1/2 flex justify-center">
+					<Link
+						className="md:absolute top-[190px] bg-white rounded-md border border-indigo-900 text-indigo-700 font-semibold py-5 md:py-2 px-8 mr-8 md:py-4 text-center my-8 md:my-0 md:mr-0 md:mb-0 hover:bg-indigo-900 hover:text-white text-sm"
+						href="/recruiter/users/create"
+					>
+						Create new User
+					</Link>
+				</div>
+				<div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-16 lg:mt-6 pb-16 w-[80%] mx-auto">
+					{props.users.length > 0 &&
+						props.users.map(user => (
+							<div
+								className="px-6 py-4 border rounded border-indigo-900 grid grid-flow-col justify-between"
+								key={user._id}
 							>
-								Edit User
-							</Link>
-						</div>
-					))}
+								<div>
+									<h3 className="text-xl">
+										{user.firstName} {user.lastName}
+									</h3>
+									<p>Role: {user.role}</p>
+								</div>
+
+								<Link
+									className="text-white bg-blue-700 rounded text-center w-full inline-block py-2 px-4 w-36 flex items-center hover:bg-blue-900"
+									href={`/recruiter/users/edit/${user._id}`}
+								>
+									Edit User
+								</Link>
+							</div>
+						))}
+				</div>
 			</div>
 		</>
 	);
