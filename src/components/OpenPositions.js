c5091702352a4b04930dc315f2a9e67f0807310d
Marija Ćehić <cehicmarija@gmail.com>
2023-04-21 21:07:15 +0200

    Update Open Positions grid

--- src/components/OpenPositions.js
+++ src/components/OpenPositions.js
@@ -8,7 +8,7 @@ function OpenPositions() {
 				Open positions
 			</h2>
 
-			<div className="grid grid-cols-2 lg:grid-cols-3 gap-4 px-12 mb-16">
+			<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 px-12 mb-16">
 				<OpenPosition
 					positionLink="#"
 					positionTitle="Front end developer"
