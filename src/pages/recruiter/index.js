3df3cc44fe1f5a48e6d9a7731ece2ae34a995f6d
Nikola Pavlović Ph.D <nikolap994@gmail.com>
2023-04-23 16:18:29 +0200

    Add homepage session info

--- src/pages/recruiter/index.js
+++ src/pages/recruiter/index.js
@@ -2,6 +2,8 @@ import Link from "next/link";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import RecruiterNavigation from "@/components/RecruiterNavigation";
+import { getSession } from "next-auth/react";
+
 import {
 	VscPerson,
 	VscOutput,
@@ -22,7 +24,7 @@ export default function Recruiter(props) {
 			<RecruiterNavigation />
 
 			<div className="max-w-7xl mx-auto">
-				<h1 className="text-5xl py-8 md:py-16 px-4 md:px-6">Welcome</h1>
+				<h1 className="text-5xl py-8 md:py-16 px-4 md:px-6">Welcome {props.firstName} {props.lastName}</h1>
 
 				<div className="grid md:grid-cols-2 items-center  gap-2 text-lg font-bold px-2 md:px-6">
 					<div className="h-24 md:h-32 lg:h-96 w-64 pl-5 lg:pl-0 lg:text-center flex items-center gap-10 w-full bg-indigo-200">
@@ -65,4 +67,23 @@ export default function Recruiter(props) {
 			</div>
 		</>
 	);
+}
+
+export async function getServerSideProps(context) {
+	const session = await getSession(context);
+
+	if (!session) {
+		return {
+			redirect: { destination: "/" },
+		};
+	} else {
+		return {
+			props: {
+				email: session._doc.email,
+				firstName: session._doc.firstName,
+				lastName: session._doc.lastName,
+				id: session._doc._id,
+			},
+		};
+	}
 }
