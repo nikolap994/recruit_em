3df9403a48f6dcda0e3b6a9568ee0709286b2392
Marija Ćehić <cehicmarija@gmail.com>
2023-04-23 13:30:36 +0200

    Add Nav items to Recruiter index, update styles

--- src/pages/recruiter/index.js
+++ src/pages/recruiter/index.js
@@ -1,9 +1,17 @@
 import Link from "next/link";
 import Head from "next/head";
-import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import RecruiterNavigation from "@/components/RecruiterNavigation";
+import {
+	VscPerson,
+	VscOutput,
+	VscReferences,
+	VscStarFull,
+} from "react-icons/vsc";
 
 export default function Recruiter(props) {
+	const router = useRouter();
+
 	return (
 		<>
 			<Head>
@@ -13,9 +21,48 @@ export default function Recruiter(props) {
 
 			<RecruiterNavigation />
 
-			<div className="max-w-7xl mx-auto px-4 md:px-6">
-				<div className="pt-10 flex flex-col md:flex-row max-w-lg justify-between md:items-center mb-8 lg:mb-12">
-					<h1 className="text-5xl my-8 md:my-16">Dashboard</h1>
+			<div className="max-w-7xl mx-auto ">
+				<h1 className="text-5xl my-8 md:my-16 px-4 md:px-6">Dashboard</h1>
+
+				<div className="px-2 md:px-6">
+					<div className="flex flex-col items-center md:flex-row gap-2 md:flex text-lg font-bold">
+						<div className="h-24 pl-5 flex items-center gap-10 w-full bg-indigo-200">
+							<Link
+								href="/recruiter/quiz"
+								className="w-full flex items-center gap-4"
+							>
+								Your Quizzes
+								<VscOutput className="h-6 w-6 animate-bounce" />
+							</Link>
+						</div>
+						<div className="h-24 pl-5 flex items-center gap-10 w-full bg-indigo-300">
+							<Link
+								href="/recruiter/positions"
+								className="w-full flex items-center gap-4"
+							>
+								Open Positions
+								<VscReferences className="h-6 w-6 animate-bounce" />
+							</Link>
+						</div>
+						<div className="h-24 pl-5 flex items-center gap-10 w-full bg-indigo-400">
+							<Link
+								href="/recruiter/reviews"
+								className="w-full flex items-center gap-4"
+							>
+								Reviews
+								<VscStarFull className="h-6 w-6 animate-spin" />
+							</Link>
+						</div>
+						<div className="h-24 pl-5 flex items-center gap-10 w-full bg-indigo-500">
+							<Link
+								href="/recruiter/users"
+								className="w-full flex items-center gap-4"
+							>
+								Users
+								<VscPerson className="h-6 w-6 animate-bounce" />
+							</Link>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
