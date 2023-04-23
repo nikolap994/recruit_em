73445398e66cc0bd2d8567d69dbfe2518823553f
Marija Ćehić <cehicmarija@gmail.com>
2023-04-23 12:51:45 +0200

    Update Resume image height

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -27,7 +27,7 @@ function Resume() {
 				onMouseEnter={() => setHover(true)}
 				onMouseLeave={() => setHover(false)}
 				onTouchMove={() => setHover(true)}
-				className={`object-contain h-[620px] lg:h-[800px] w-full absolute top-0 lg:w-1/2 top-[21rem] lg:top-[15rem] xl:top-[18.8rem] lg:left-[25%] transition-opacity ${
+				className={`object-contain h-[620px] lg:h-[800px] w-full absolute top-0 lg:w-1/2 top-[17rem] lg:top-[15rem] xl:top-[18.8rem] lg:left-[25%] transition-opacity ${
 					isHovered ? "opacity-0" : "opacity-1"
 				}`}
 				src={cvImage2}
