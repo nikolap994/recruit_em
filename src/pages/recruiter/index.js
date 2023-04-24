c563c94b3696b616170e5c053f80d6c4ecb88ec7
Marija Ćehić <cehicmarija@gmail.com>
2023-04-24 20:26:06 +0200

    Update Recruiter dashboard styles

--- src/pages/recruiter/index.js
+++ src/pages/recruiter/index.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Head from "next/head";
+import { getSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import RecruiterNavigation from "@/components/RecruiterNavigation";
-import { getSession } from "next-auth/react";
 
 import {
 	VscPerson,
@@ -24,50 +24,42 @@ export default function Recruiter(props) {
 			<RecruiterNavigation />
 
 			<div className="max-w-7xl mx-auto">
-				<h1 className="text-2xl md:text-4xl py-8 md:py-16 px-4 md:px-12">
+				<h1 className="text-2xl md:text-4xl py-8 md:py-16 mx-10 md:px-12">
 					Welcome,
 					<span className="pl-2 text-indigo-500">
 						{props.firstName} {props.lastName}
 					</span>
 				</h1>
 
-				<div className="grid md:grid-cols-2 items-center gap-2 text-lg font-bold px-2 md:px-12">
-					<div className="h-24 md:h-32 lg:h-96 w-64 pl-5 lg:pl-0 lg:text-center flex items-center gap-10 w-full bg-indigo-200">
-						<Link
-							href="/recruiter/quiz"
-							className="w-full flex items-center gap-4 justify-center"
-						>
-							Your Quizzes
-							<VscOutput className="h-6 w-6 animate-bounce lg:animate-none lg:hover-animate-bounce" />
-						</Link>
-					</div>
-					<div className="h-24 md:h-32 lg:h-96 w-64 pl-5 lg:pl-0 lg:text-center flex items-center gap-10 w-full bg-indigo-300 lg:bg-indigo-400">
-						<Link
-							href="/recruiter/positions"
-							className="w-full flex items-center gap-4 justify-center"
-						>
-							Open Positions
-							<VscReferences className="h-6 w-6 animate-bounce lg:animate-none lg:hover-animate-bounce" />
-						</Link>
-					</div>
-					<div className="h-24 md:h-32 lg:h-96 w-64 pl-5 lg:pl-0 lg:text-center flex items-center gap-10 w-full bg-indigo-400">
-						<Link
-							href="/recruiter/reviews"
-							className="w-full flex items-center gap-4 justify-center"
-						>
-							Reviews
-							<VscStarFull className="h-6 w-6 animate-spin lg:animate-none lg:hover-animate-spin" />
-						</Link>
-					</div>
-					<div className="h-24 md:h-32 lg:h-96 w-64 pl-5 lg:pl-0 lg:text-center flex items-center gap-10 w-full bg-indigo-500 lg:bg-indigo-200">
-						<Link
-							href="/recruiter/users"
-							className="w-full flex items-center gap-4 justify-center"
-						>
-							Users
-							<VscPerson className="h-6 w-6 animate-bounce lg:animate-none lg:hover-animate-bounce" />
-						</Link>
-					</div>
+				<div className="grid md:grid-cols-2 items-center gap-2 text-lg font-bold px-2 md:px-12 lg:mb-16 mx-8">
+					<Link
+						href="/recruiter/quiz"
+						className="w-full flex items-center gap-4 justify-center h-24 md:h-32 lg:h-96 w-64 pl-5 lg:pl-0 lg:text-center flex items-center gap-10 w-full bg-indigo-200"
+					>
+						Your Quizzes
+						<VscOutput className="h-6 w-6 animate-bounce lg:animate-none lg:hover-animate-bounce" />
+					</Link>
+					<Link
+						href="/recruiter/positions"
+						className="flex items-center justify-center h-24 md:h-32 lg:h-96 w-64 pl-5 lg:pl-0 lg:text-center gap-10 w-full bg-indigo-300 lg:bg-indigo-400"
+					>
+						Open Positions
+						<VscReferences className="h-6 w-6 animate-bounce lg:animate-none lg:hover-animate-bounce" />
+					</Link>
+					<Link
+						href="/recruiter/reviews"
+						className="justify-center h-24 md:h-32 lg:h-96 w-64 pl-5 lg:pl-0 lg:text-center flex items-center gap-10 w-full bg-indigo-400"
+					>
+						Reviews
+						<VscStarFull className="h-6 w-6 animate-spin lg:animate-none lg:hover-animate-spin" />
+					</Link>
+					<Link
+						href="/recruiter/users"
+						className="justify-center h-24 md:h-32 lg:h-96 w-64 pl-5 lg:pl-0 lg:text-center flex items-center gap-10 w-full bg-indigo-500 lg:bg-indigo-200"
+					>
+						Users
+						<VscPerson className="h-6 w-6 animate-bounce lg:animate-none lg:hover-animate-bounce" />
+					</Link>
 				</div>
 			</div>
 		</>
