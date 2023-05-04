1a3914f30ea91fbcb4163346dbbc16e72bc48190
Marija Ćehić <cehicmarija@gmail.com>
2023-05-04 22:26:24 +0200

    Update Reviews dashboard margin/padding

--- src/pages/recruiter/reviews/index.js
+++ src/pages/recruiter/reviews/index.js
@@ -14,17 +14,17 @@ export default function Reviews(props) {
 			<RecruiterNavigation />
 
 			<div className="max-w-7xl mx-auto px-4 md:px-12">
-				<div className="pt-10 flex flex-col md:flex-row max-w-lg justify-between md:items-center mb-8 lg:mb-12">
-					<h1 className="text-5xl my-8 md:my-16">Reviews Dashboard</h1>
+				<div className="pt-10 flex flex-col md:flex-row max-w-lg justify-between md:items-center lg:mb-12">
+					<h1 className="text-5xl my-8">Reviews Dashboard</h1>
 				</div>
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5 pb-16">
 					{props.reviews.length > 0 &&
 						props.reviews.map((review) => (
-							<div className="px-6 py-4" key={review._id}>
+							<div className="pb-6" key={review._id}>
 								<p>{review.user}</p>
 								<p>{review.position}</p>
 								<Link
-									className="text-white bg-blue-700 rounded text-center w-full inline-block pt-2 pb-2"
+									className="text-white bg-blue-700 rounded text-center w-full inline-block py-2 mt-7"
 									href={`/recruiter/reviews/edit/${review._id}`}
 								>
 									Edit Review
