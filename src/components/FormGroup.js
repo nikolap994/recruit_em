4c2f38d18261cd3e7dd24e642a1e1cac880dc9ba
Marija Ćehić <cehicmarija@gmail.com>
2023-05-04 23:33:57 +0200

    Update Form Group responsive wifth

--- src/components/FormGroup.js
+++ src/components/FormGroup.js
@@ -18,7 +18,7 @@ export default function FormGroup(props, { innerRef }) {
 		<div>
 			<label htmlFor={id}>{label}</label>
 			<input
-				className="border border-indigo-800 border-1 ml-5 w-[60vw] max-w-[300px] mb-6"
+				className="border border-indigo-800 border-1 ml-5 w-[60vw] max-w-[380px] mb-6"
 				required
 				id={id}
 				name={name}
