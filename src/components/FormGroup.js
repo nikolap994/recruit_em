b123ba1914feee3c4d1f8a96300dc425ab100074
Marija Ćehić <cehicmarija@gmail.com>
2023-05-14 19:40:05 +0200

    Update New Quiz info styles

--- src/components/FormGroup.js
+++ src/components/FormGroup.js
@@ -15,10 +15,10 @@ export default function FormGroup(props, { innerRef }) {
 	} = props;
 
 	return (
-		<div>
+		<div className="flex justify-between">
 			<label htmlFor={id}>{label}</label>
 			<input
-				className="border border-indigo-800 border-1 ml-5 w-[60vw] max-w-[380px] mb-6"
+				className="border border-indigo-800 border-1 ml-5 w-[60vw] max-w-[380px] mb-6 py-4 pl-4"
 				required
 				id={id}
 				name={name}
